700b597a0f2cab0bc7919053b5b40eb48928cf92
Bart van Tuijn <bart@bartvantuijn.nl>
2023-07-08 22:55:52 +0200

    1.1.2 - Change block-posts.php layout

--- wp-content/themes/standardwp/template-parts/blocks/block-posts.php
+++ wp-content/themes/standardwp/template-parts/blocks/block-posts.php
@@ -22,9 +22,9 @@ if ( $posts ) : ?>
                     setup_postdata($post); ?>
 
                     <div class="col-lg card border-0">
-                        <a href="#" class="card-image-top p-5" style="min-height:350px;background-image:url('<?php echo wp_get_attachment_image_url( get_post_thumbnail_id($post->ID), 'full') ?: bloginfo('template_url') . '/img/placeholder.png'; ?>');background-origin:content-box;background-size:contain;background-position:center;background-repeat:no-repeat;background-blend-mode:multiply;"></a>
+                        <a href="#" class="card-image-top" style="min-height:350px;background-image:url('<?php echo wp_get_attachment_image_url( get_post_thumbnail_id($post->ID), 'full') ?: bloginfo('template_url') . '/img/placeholder.png'; ?>');background-size:cover;background-position:center;background-repeat:no-repeat;"></a>
                         <div class="card-body text-body">
-                            <h4><?php the_title(); ?></h4>
+                            <h5><?php the_title(); ?></h5>
                             <?php if ( get_post_type() == 'product' && class_exists( 'woocommerce' ) ) :
                                 $product = wc_get_product( get_the_id() ); ?>
                                 <span class="price" style="display:block;"><?php echo wc_price($product->get_price()); ?></span>
