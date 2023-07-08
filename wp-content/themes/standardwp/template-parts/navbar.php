d6f07331cdf76bb63dd0944748934bc25cc38f89
Bart van Tuijn <bart@bartvantuijn.nl>
2023-07-09 00:37:13 +0200

    1.3.0 - Calculate negative margin with floating menu

--- wp-content/themes/standardwp/template-parts/navbar.php
+++ wp-content/themes/standardwp/template-parts/navbar.php
@@ -1,137 +1,149 @@
-<?php
+<div id="nav-wrapper">
 
-$headerTopMenu = wp_get_nav_menu_object('header-topmenu');
-$headerTopMenuAlignment = get_field('header_topmenu_uitlijnen', $headerTopMenu);
+    <?php
 
-if ( has_nav_menu( 'header-topmenu' ) ) : ?>
+    $headerTopMenu = wp_get_nav_menu_object('header-topmenu');
+    $headerTopMenuAlignment = get_field('header_topmenu_uitlijnen', $headerTopMenu);
 
-    <div class="navbar navbar-expand bg-primary" style="z-index:9999;">
-        <div class="container">
+    if ( has_nav_menu( 'header-topmenu' ) ) : ?>
 
-            <?php
-                wp_nav_menu( array(
-                    'theme_location'  => 'header-topmenu',
-                    'depth'           => 2,
-                    'container'       => '',
-                    'container_class' => '',
-                    'container_id'    => 'header-topmenu',
-                    'menu_class'      => 'navbar-nav ' . $headerTopMenuAlignment,
-                    'fallback_cb'     => 'WP_Bootstrap_Navwalker::fallback',
-                    'walker'          => new WP_Bootstrap_Navwalker(),
-                ) );
-            ?>
-
-        </div>
-    </div>
-
-<?php endif; ?>
-
-<?php
-
-$headerMenu = wp_get_nav_menu_object('header-menu');
-$headerMenuFloat = get_field('header_menu_zweven', $headerMenu); ?>
-
-<div id="nav-wrapper" class="sticky-top" style="margin-bottom:<?php echo $headerMenuFloat ? '-8.5rem' : '0rem'; ?>;">
-
-    <?php if ( has_nav_menu( 'header-menu' ) ) : ?>
-
-        <nav class="navbar navbar-expand-lg <?php echo ! $headerMenuFloat ? 'bg-white' : ''; ?> shadow py-3">
-            <div class="container position-relative">
+        <div class="navbar navbar-expand bg-primary" style="z-index:9999;">
+            <div class="container">
 
                 <?php
-
-                $headerMenuLogo = get_field('header_menu_logo', $headerMenu);
-                $headerMenuLogoCenter = get_field('header_menu_logo_centreren', $headerMenu);
-                $headerMenuAlignment = get_field('header_menu_uitlijnen', $headerMenu);
-
-                if ( $headerMenuLogo ) : ?>
-
-                    <a class="navbar-brand p-0 <?php echo $headerMenuLogoCenter ? 'position-absolute top-0 start-50 translate-middle-x' : ''; ?>" href="<?php bloginfo('url'); ?>" title="<?php bloginfo('name'); ?>">
-                        <?php echo wp_get_attachment_image( $headerMenuLogo, 'full', '', array('class' => 'img-fluid', 'loading' => 'eager') ); ?>
-                    </a>
-
-                <?php else : ?>
-
-                    <a class="navbar-brand p-0 <?php echo $headerMenuLogoCenter ? 'position-absolute top-0 start-50 translate-middle-x' : ''; ?>" href="<?php bloginfo('url'); ?>" title="<?php bloginfo('name'); ?>" style="line-height:37px;">
-                        <?php bloginfo('name'); ?>
-                    </a>
-
-                <?php endif; ?>
-
-                <button class="navbar-toggler <?php echo $headerMenuAlignment; ?>" type="button" data-bs-toggle="collapse" data-bs-target="#navbarMenuSupportedContent" aria-controls="navbarMenuSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-
-                <div class="collapse navbar-collapse" id="navbarMenuSupportedContent">
-                    <?php
-                        wp_nav_menu( array(
-                            'theme_location'  => 'header-menu',
-                            'depth'           => 2,
-                            'container'       => '',
-                            'container_class' => '',
-                            'container_id'    => 'header-menu',
-                            'menu_class'      => 'navbar-nav ' . $headerMenuAlignment,
-                            'fallback_cb'     => 'WP_Bootstrap_Navwalker::fallback',
-                            'walker'          => new WP_Bootstrap_Navwalker(),
-                        ) );
-                    ?>
-                </div>
+                    wp_nav_menu( array(
+                        'theme_location'  => 'header-topmenu',
+                        'depth'           => 2,
+                        'container'       => '',
+                        'container_class' => '',
+                        'container_id'    => 'header-topmenu',
+                        'menu_class'      => 'navbar-nav ' . $headerTopMenuAlignment,
+                        'fallback_cb'     => 'WP_Bootstrap_Navwalker::fallback',
+                        'walker'          => new WP_Bootstrap_Navwalker(),
+                    ) );
+                ?>
 
             </div>
-        </nav>
+        </div>
 
     <?php endif; ?>
 
     <?php
 
-    $headerSubMenu = wp_get_nav_menu_object('header-submenu');
-    $headerSubMenuScrollbar = get_field('header_submenu_scrollbar', $headerSubMenu);
+    $headerMenu = wp_get_nav_menu_object('header-menu');
+    $headerMenuFloat = get_field('header_menu_zweven', $headerMenu); ?>
 
-    if ( has_nav_menu( 'header-submenu' ) ) : ?>
+    <div id="menu-wrapper" class="sticky-top">
 
-        <div class="navbar navbar-expand-lg bg-secondary">
-            <div class="container">
+        <?php if ( has_nav_menu( 'header-menu' ) ) : ?>
 
-                <button class="navbar-toggler <?php echo $headerMenuAlignment; ?>" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSubMenuSupportedContent" aria-controls="navbarSubMenuSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
+            <nav class="navbar navbar-expand-lg <?php echo ! $headerMenuFloat ? 'bg-white' : ''; ?> shadow py-3">
+                <div class="container position-relative">
 
-                <div class="collapse navbar-collapse" id="navbarSubMenuSupportedContent">
                     <?php
-                    wp_nav_menu( array(
-                        'theme_location'  => 'header-submenu',
-                        'depth'           => 2,
-                        'container'       => '',
-                        'container_class' => '',
-                        'container_id'    => 'header-submenu',
-                        'menu_class'      => 'navbar-nav',
-                        'fallback_cb'     => 'WP_Bootstrap_Navwalker::fallback',
-                        'walker'          => new WP_Bootstrap_Navwalker(),
-                    ) );
-                    get_search_form();
-                    ?>
+
+                    $headerMenuLogo = get_field('header_menu_logo', $headerMenu);
+                    $headerMenuLogoCenter = get_field('header_menu_logo_centreren', $headerMenu);
+                    $headerMenuAlignment = get_field('header_menu_uitlijnen', $headerMenu);
+
+                    if ( $headerMenuLogo ) : ?>
+
+                        <a class="navbar-brand p-0 <?php echo $headerMenuLogoCenter ? 'position-absolute top-0 start-50 translate-middle-x' : ''; ?>" href="<?php bloginfo('url'); ?>" title="<?php bloginfo('name'); ?>">
+                            <?php echo wp_get_attachment_image( $headerMenuLogo, 'full', '', array('class' => 'img-fluid', 'loading' => 'eager') ); ?>
+                        </a>
+
+                    <?php else : ?>
+
+                        <a class="navbar-brand p-0 <?php echo $headerMenuLogoCenter ? 'position-absolute top-0 start-50 translate-middle-x' : ''; ?>" href="<?php bloginfo('url'); ?>" title="<?php bloginfo('name'); ?>" style="line-height:37px;">
+                            <?php bloginfo('name'); ?>
+                        </a>
+
+                    <?php endif; ?>
+
+                    <button class="navbar-toggler <?php echo $headerMenuAlignment; ?>" type="button" data-bs-toggle="collapse" data-bs-target="#navbarMenuSupportedContent" aria-controls="navbarMenuSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+
+                    <div class="collapse navbar-collapse" id="navbarMenuSupportedContent">
+                        <?php
+                            wp_nav_menu( array(
+                                'theme_location'  => 'header-menu',
+                                'depth'           => 2,
+                                'container'       => '',
+                                'container_class' => '',
+                                'container_id'    => 'header-menu',
+                                'menu_class'      => 'navbar-nav ' . $headerMenuAlignment,
+                                'fallback_cb'     => 'WP_Bootstrap_Navwalker::fallback',
+                                'walker'          => new WP_Bootstrap_Navwalker(),
+                            ) );
+                        ?>
+                    </div>
+
                 </div>
-
-            </div>
-        </div>
-
-        <?php if ( $headerSubMenuScrollbar ) : ?>
-
-            <div class="progress" role="progressbar" aria-label="Scrollbar" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100" style="height:3px;border-radius:0;--bs-progress-bar-transition:width .1s linear;">
-                <div id="scrollbar" class="progress-bar bg-primary"></div>
-            </div>
-
-            <script>
-                $(document).scroll(function () {
-                    var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
-                    var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
-                    var scrolled = (winScroll / height) * 100;
-                    document.getElementById("scrollbar").style.width = scrolled + "%";
-                });
-            </script>
+            </nav>
 
         <?php endif; ?>
 
-    <?php endif; ?>
+        <?php
+
+        $headerSubMenu = wp_get_nav_menu_object('header-submenu');
+        $headerSubMenuScrollbar = get_field('header_submenu_scrollbar', $headerSubMenu);
+
+        if ( has_nav_menu( 'header-submenu' ) ) : ?>
+
+            <div class="navbar navbar-expand-lg bg-secondary">
+                <div class="container">
+
+                    <button class="navbar-toggler <?php echo $headerMenuAlignment; ?>" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSubMenuSupportedContent" aria-controls="navbarSubMenuSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+
+                    <div class="collapse navbar-collapse" id="navbarSubMenuSupportedContent">
+                        <?php
+                        wp_nav_menu( array(
+                            'theme_location'  => 'header-submenu',
+                            'depth'           => 2,
+                            'container'       => '',
+                            'container_class' => '',
+                            'container_id'    => 'header-submenu',
+                            'menu_class'      => 'navbar-nav',
+                            'fallback_cb'     => 'WP_Bootstrap_Navwalker::fallback',
+                            'walker'          => new WP_Bootstrap_Navwalker(),
+                        ) );
+                        get_search_form();
+                        ?>
+                    </div>
+
+                </div>
+            </div>
+
+            <?php if ( $headerSubMenuScrollbar ) : ?>
+
+                <div class="progress" role="progressbar" aria-label="Scrollbar" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100" style="height:3px;border-radius:0;--bs-progress-bar-transition:width .1s linear;">
+                    <div id="scrollbar" class="progress-bar bg-primary"></div>
+                </div>
+
+                <script>
+                    $(document).scroll(function () {
+                        var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+                        var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+                        var scrolled = (winScroll / height) * 100;
+                        document.getElementById("scrollbar").style.width = scrolled + "%";
+                    });
+                </script>
+
+            <?php endif; ?>
+
+        <?php endif; ?>
+
+    </div>
 
 </div>
+
+<?php if ( $headerMenuFloat ) : ?>
+
+    <script type="text/javascript">
+        $('#menu-wrapper').css('margin-bottom', -$('#nav-wrapper').height());
+    </script>
+
+<?php endif; ?>
