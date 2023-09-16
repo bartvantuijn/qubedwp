92327155dffc0839739730295f361f1b927aea49
Bart van Tuijn <bart@bartvantuijn.nl>
2023-09-16 23:17:58 +0200

    1.3.3 - Change block-posts.php

--- wp-content/themes/standardwp/template-parts/blocks/block-posts.php
+++ wp-content/themes/standardwp/template-parts/blocks/block-posts.php
@@ -21,16 +21,18 @@ if ( $posts ) : ?>
                     // Setup this post for WP functions (variable must be named $post).
                     setup_postdata($post); ?>
 
-                    <div class="col-lg card border-0">
-                        <a href="#" class="card-image-top" style="min-height:350px;background-image:url('<?php echo wp_get_attachment_image_url( get_post_thumbnail_id($post->ID), 'full') ?: bloginfo('template_url') . '/img/placeholder.png'; ?>');background-size:cover;background-position:center;background-repeat:no-repeat;"></a>
-                        <div class="card-body text-body">
-                            <h5><?php the_title(); ?></h5>
-                            <?php if ( get_post_type() == 'product' && class_exists( 'woocommerce' ) ) :
-                                $product = wc_get_product( get_the_id() ); ?>
-                                <span class="price" style="display:block;"><?php echo wc_price($product->get_price()); ?></span>
-                            <?php endif; ?>
-                            <?php /* <p class="card-text"><?php the_excerpt(); ?></p> */ ?>
-                            <a href="<?php the_permalink(); ?>" class="btn btn-primary">Bekijk <?php echo strtolower(get_post_type_object(get_post_type($post->ID))->labels->singular_name); ?></a>
+                    <div class="col-6 col-md-3">
+                        <div class="card border-0">
+                            <a href="<?php the_permalink(); ?>" class="card-image-top" style="min-height:350px;background-image:url('<?php echo wp_get_attachment_image_url( get_post_thumbnail_id($post->ID), 'full') ?: bloginfo('template_url') . '/img/placeholder.png'; ?>');background-size:cover;background-position:center;background-repeat:no-repeat;"></a>
+                            <div class="card-body text-body">
+                                <h2 class="card-title h6"><?php the_title(); ?></h2>
+                                <?php if ( get_post_type() == 'product' && class_exists( 'woocommerce' ) ) :
+                                    $product = wc_get_product( get_the_id() ); ?>
+                                    <span class="price" style="display:block;"><?php echo wc_price($product->get_price()); ?></span>
+                                <?php endif; ?>
+                                <?php /* <p class="card-text"><?php the_excerpt(); ?></p> */ ?>
+                                <a href="<?php the_permalink(); ?>" class="btn btn-primary">Bekijk <?php echo strtolower(get_post_type_object(get_post_type($post->ID))->labels->singular_name); ?></a>
+                            </div>
                         </div>
                     </div>
 
