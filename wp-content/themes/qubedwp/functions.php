050b8215728d72c63ca1f974d04b5b2df5dffdec
Bart van Tuijn <bart@bartvantuijn.nl>
2023-12-16 23:41:43 +0100

    2.8.5 - Queue parent and child style.css

--- wp-content/themes/qubedwp/functions.php
+++ wp-content/themes/qubedwp/functions.php
@@ -13,6 +13,13 @@ function custom_theme_setup() {
 }
 add_action( 'after_setup_theme', 'custom_theme_setup' );
 
+//Queue parent and child style
+function custom_theme_styles() {
+    wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+    wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css', array('parent-style') );
+}
+add_action( 'wp_enqueue_scripts', 'custom_theme_styles' );
+
 //Register Menus
 function register_menus() {
 
